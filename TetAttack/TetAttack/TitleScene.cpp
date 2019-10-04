5347481f10b8c6f1a04e0cb1ed743c3318c0ac6d
include-iinteresting <game_love_h@outlook.jp>
2019-10-04 10:19:01 +0900

    TitleScene Add TitleBG

--- TetAttack/TetAttack/TitleScene.cpp
+++ TetAttack/TetAttack/TitleScene.cpp
@@ -1,12 +1,19 @@
 #include "stdafx.h"
 #include "TitleScene.h"
 #include <d2d1.h>
+#include "TextureLoader.h"
+
+#define BG_TEXTURE_PATH _T("res/TitleBG.png")
+
+#undef SAFE_RELEASE
+#define SAFE_RELEASE(o) if(o) { (o)->Release(); o = NULL; };
 
 /**
 * @brief	コンストラクタ
 */
-CTitleScene::CTitleScene()
+CTitleScene::CTitleScene(ID2D1RenderTarget *pRenderTarget)
 {
+	InitTexture(pRenderTarget);
 }
 
 
@@ -35,8 +42,6 @@ void CTitleScene::draw(ID2D1RenderTarget * pRenderTarget)
 {
 	ID2D1SolidColorBrush *pBrush = NULL;
 
-	pRenderTarget->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::Yellow), &pBrush);
-
 	D2D1_SIZE_F size = pRenderTarget->GetSize();
 	D2D1_RECT_F rc;
 	rc.left = 0.0f;
@@ -44,11 +49,16 @@ void CTitleScene::draw(ID2D1RenderTarget * pRenderTarget)
 	rc.right = size.width;
 	rc.bottom = size.height;
 
-	pRenderTarget->FillRectangle(rc, pBrush);
+	if (m_pBGImage)
+		pRenderTarget->DrawBitmap(m_pBGImage, rc, 1.0f, D2D1_BITMAP_INTERPOLATION_MODE::D2D1_BITMAP_INTERPOLATION_MODE_LINEAR, NULL);
+}
 
-	if (pBrush)
-	{
-		pBrush->Release();
-		pBrush = NULL;
-	}
+
+/**
+* @brief	画像に関する初期化
+*/
+void CTitleScene::InitTexture(ID2D1RenderTarget *pRenderTarget)
+{
+	m_pBGImage = NULL;
+	CTextureLoader::CreateD2D1BitmapFromFile(pRenderTarget, BG_TEXTURE_PATH, &m_pBGImage);
 }
--- TetAttack/TetAttack/TitleScene.h
+++ TetAttack/TetAttack/TitleScene.h
@@ -1,14 +1,19 @@
 #pragma once
 #include "IGameScene.h"
 
+struct ID2D1Bitmap;
 
 class CTitleScene :
 	public IGameScene
 {
 public:
-	CTitleScene();
+	CTitleScene(ID2D1RenderTarget *pRenderTarget);
 	virtual ~CTitleScene();
 	virtual	GameSceneResultCode	move()	override;
 	virtual	void	draw(ID2D1RenderTarget *pRenderTarget)	override;
+private:
+	void	InitTexture(ID2D1RenderTarget *pRenderTarget);
+private:
+	ID2D1Bitmap * m_pBGImage;
 };
 
